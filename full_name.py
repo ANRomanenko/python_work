fdaaf47dfff1f2ce20dd8e37d228f90562d0f500
romanenko <lifeapricot@gmail.com>
2023-03-22 16:48:01 +0200

    new files

--- full_name.py
+++ full_name.py
@@ -12,3 +12,12 @@ print("Python")
 print("\tPython\n\tHello")
 print()
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
+print()
+
+favorite_language = ' python'
+favorite_language = favorite_language.lstrip()
+print(favorite_language)
+print()
+left_right = ' PYTHON '
+left_right = left_right.strip()
+print(left_right)
---
+++ name_cases.py
@@ -0,0 +1,16 @@
+name = 'Andrey'
+print(f'Hello {name.upper()}, would you like to learn some Python today?')
+print(f'Hello {name.lower()}, would you like to learn some Python today?')
+print(f'Hello {name.title()}, would you like to learn some Python today?')
+
+
+albert = '\tAlbert Einstein once said, "A person who never made\n\ta mistake never tried anything new."'
+print(albert)
+print()
+famous_person = '\tAlbert Einstein'
+message = 'once said, "A person who never made\n\ta mistake never tried anything new."'
+print(famous_person, message)
+print()
+name_2 = '\t         Albert Einstein  \n     '
+print(famous_person.strip())
+
