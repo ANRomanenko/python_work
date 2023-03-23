4f18814ee95aa9192058c0d6f30d1350f0e93274
romanenko <lifeapricot@gmail.com>
2023-03-23 16:49:46 +0200

    new files

--- name_cases.py
+++ name_cases.py
@@ -1,3 +1,7 @@
+import this
+# The Zen of Python, by Tim Peters
+# Красивое лучше, чем уродливое.
+print()
 name = 'Andrey'
 print(f'Hello {name.upper()}, would you like to learn some Python today?')
 print(f'Hello {name.lower()}, would you like to learn some Python today?')
@@ -13,4 +17,35 @@ print(famous_person, message)
 print()
 name_2 = '\t         Albert Einstein  \n     '
 print(famous_person.strip())
+print()
+print(10 ** 3) # Возведение в степень
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+print(0.1 + 0.1) # Числа с плавающей точкой (Вещественные числа)
+print(2 * 0.2) # Числа с плавающей точкой (Вещественные числа)
+print(4 / 2)
+print(1 + 2.0)
+print(3.0 ** 2)
+
+universe_age = 14_000_000_000 # Символы подчеркивания в числах
+print(universe_age)
+print()
+# Множественное присваивание
+x, y, z = 0, 0, 0
+print(x, y, z)
+print()
+# Константы
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+print()
+print(5 + 3)
+print(4 * 2)
+print(10 - 2)
+print(16 / 2)
+print()
+
+number = 1
+text = f'I number {number}'
+print(text)
 
