ec8ee507c92b94e171add8af34805b9f4cff1e16
andredance <lifeapricot@gmail.com>
2023-03-28 23:41:22 +0300

    new files

---
+++ first_numbers.py
@@ -0,0 +1,6 @@
+numbers = list(range(1, 5))
+print(numbers)
+print()
+
+for nums in range(1, 10 + 1):
+    print(nums)
---
+++ magicians.py
@@ -0,0 +1,20 @@
+# Цикл for
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!\n\n")
+
+pizzas = ['parmezan', 'cherry', 'chees', 'pomidor']
+for pizza in pizzas:
+    print(f'Моя из любимых пицц: это {pizza.title()}.\n')
+
+print("I really love pizza!\n\n")
+
+animals = ['cat', 'dog', 'lioness']
+for animal in animals:
+    print(f'{animal.title()} A would make a great pet\n')
+
+print("Any of these animals would make a great pet!")
