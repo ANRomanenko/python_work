8f3532c52f8ad3cc849738e7f662d1c0e82772e9
andredance <lifeapricot@gmail.com>
2023-04-04 23:44:48 +0300

    new files

--- alien.py
+++ alien.py
@@ -1,6 +1,75 @@
 # Словари
+# В Python словарь заключается в фигурные скобки {}, в которых приводится последовательность пар «ключ-значение»,
 
 alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f'\nYou just earned {new_points} points!\n')
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+print()
+
+# Создание пустого словаря
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+print()
+
+# Изменение значений в словаре
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}")
+print()
+
+# Скорость пришельца
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+alien_0['speed'] = 'fast'
+print(f"Original position: {alien_0['x_position']}")
+
+# Пришелец перемещается вправо.
+# Вычесляем величину смещения на основании текущей скорости.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # Пришелец двигается быстро
+    x_increment = 3
+
+# Новая позиция равна сумме старой позиции и приращения.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+print()
+
+# Удаление пар «ключ-значение»
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+print()
+
+# Словарь с однотипными объектами
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+print(favorite_languages['jen'])
---
+++ alien_no_points.py
@@ -0,0 +1,7 @@
+# Обращение к значениям методом get()
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+# print(alien_0['points'])
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
